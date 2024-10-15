ccaf58b1c1882d7bc7f70f014143ca729315db21
Rory Campbell <roryc89@googlemail.com>
2024-10-15 18:45:18 +0200

    adds extension body

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
 import { workspace, ExtensionContext, window, commands } from 'vscode';
 import {
 	LanguageClient,
@@ -9,25 +6,84 @@ import {
 } from 'vscode-languageclient/node';
 let client: LanguageClient;
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: ExtensionContext) {
+export async function activate(context: ExtensionContext) {
+	const name = 'purescript';
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	const serverOptions: ServerOptions = {
+		command: "purs",
+		args: ["lsp", "server" , 
+      "--output-directory", "./output",
+    ],
+	};
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "purescript-fast-ide-vscode" is now active!');
+	const outputChannel = window.createOutputChannel(name);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = commands.registerCommand('purescript-fast-ide-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		window.showInformationMessage('Hello World from purescript-fast-ide-vscode!');
-	});
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [
+			{
+				scheme: 'file',
+				language: 'purescript',
+				pattern: '**/*.purs'
+			}],
 
-	context.subscriptions.push(disposable);
+		synchronize: {
+			configurationSection: 'purescript-lsp',
+			fileEvents:
+				[
+					workspace.createFileSystemWatcher('**/*.purs'),
+					workspace.createFileSystemWatcher('**/*.js')
+				]
+		},
+    outputChannel,
+		diagnosticCollectionName: name,
+		outputChannelName: name,
+		diagnosticPullOptions:
+		{
+			onChange: false,
+			onSave: true,
+		}
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'purescript',
+		name,
+		serverOptions,
+		clientOptions
+	);
+
+	context.subscriptions.push(commands.registerCommand('purescript.build', () => {
+		client.sendRequest('build');
+	}));
+	context.subscriptions.push(commands.registerCommand('purescript.clear-cache', () => {
+		client.sendRequest('clear-cache');
+	}));
+	context.subscriptions.push(commands.registerCommand('purescript.clear-cache:exports', () => {
+		client.sendRequest('clear-cache:exports');
+	}));
+	context.subscriptions.push(commands.registerCommand('purescript.clear-cache:rebuilds', () => {
+		client.sendRequest('clear-cache:rebuilds');
+	}));
+	context.subscriptions.push(commands.registerCommand('purescript.delete-output', () => {
+		client.sendRequest('delete-output');
+	}));
+	context.subscriptions.push(commands.registerCommand('purescript.index-fast', () => {
+		client.sendRequest('index-fast');
+	}));
+	context.subscriptions.push(commands.registerCommand('purescript.index-full', () => {
+		client.sendRequest('index-full');
+	}));
+
+	// Start the client. This will also launch the server
+	await client.start();
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
